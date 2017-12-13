844f4d2541877ea53cd2e40ea89b1c9227868908
user <user@users-Mac-mini.localdomain>
2017-12-13 12:00:59 +0200

    Initial Commit

---
+++ Learn SlangTests/Learn_SlangTests.swift
@@ -0,0 +1,36 @@
+//
+//  Learn_SlangTests.swift
+//  Learn SlangTests
+//
+//  Created by user on 13/12/2017.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+import XCTest
+@testable import Learn_Slang
+
+class Learn_SlangTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
